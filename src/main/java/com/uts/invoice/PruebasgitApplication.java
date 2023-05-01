c9c2d3edd513b12e29dacdd45ff360068c921521
MiguelOssesR <miguelossesr@gmail.com>
2023-04-30 19:48:41 -0500

    Primer Push

---
+++ src/main/java/com/uts/invoice/PruebasgitApplication.java
@@ -0,0 +1,13 @@
+package com.uts.invoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebasgitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebasgitApplication.class, args);
+	}
+
+}
